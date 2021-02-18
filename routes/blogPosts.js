ffd023302109a14563f88dd7d60c91214ebb1080
danisylvester <maria.sylvester10@gmail.com>
2021-02-18 17:09:30 -0600

    Added middleware to change the number of likes on a blogpost.

--- routes/blogPosts.js
+++ routes/blogPosts.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 
 const router = express.Router();
 
-// @desc get blog post by id
+// @desc GET BLOG POST BY ID
 // @route GET /blogposts/:id
 router.get('/:id', (req, res) => {
     const blogPostID = req.params.id;
@@ -19,7 +19,7 @@ router.get('/:id', (req, res) => {
         })
 })
 
-// @desc get all blog posts
+// @desc GET ALL BLOG POSTS
 // @route GET /blogposts
 router.get('/', (req, res) => {
     BlogPost.find()
@@ -32,14 +32,17 @@ router.get('/', (req, res) => {
         })
 })
 
-// @desc add a new comment
+// @desc ADD A NEW COMMENT TO BLOG POST
 // @route PUT /blogposts/:id
-router.put('/:id', (req, res) => {
+router.put('/:id/comments', (req, res) => {
     const blogID = req.params.id;
-    BlogPost.findByIdAndUpdate(blogID, 
-        {$push : {
-            'comments' : req.body
-        }})
+    BlogPost.findByIdAndUpdate(blogID,
+        {
+            $push : {
+                'comments': req.body
+            }
+        },
+        {new: true}) //Changes default, so function returns updated doc - not original
         .exec()
         .then(doc => {
             res.status(200).json(doc);
@@ -49,6 +52,24 @@ router.put('/:id', (req, res) => {
         })
 })
 
-// @desc update the number of likes
+// @desc UPDATE NUMBER OF LIKES ON BLOG POST
+// @route PUT /blogpost/:id
+router.put('/:id/likes', (req, res) => {
+    const blogId = req.params.id;
+    BlogPost.findByIdAndUpdate(blogId, 
+        {
+            $set: {
+                'likes' : req.body.likes
+            }
+        },
+        {new: true}) //Changes default, so function returns updated doc - not original
+        .exec()
+        .then(doc => {
+            res.status(200).json(doc);
+        })
+        .catch(err =>{
+            res.status(500).json({error : err});
+        })
+})
 
 module.exports = router;
