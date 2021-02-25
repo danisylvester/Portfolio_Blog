bbc699e3b9a2dca43e682d44fed4b15f2daa64e5
danisylvester <maria.sylvester10@gmail.com>
2021-02-24 21:47:03 -0600

    Created user model and passed it to AdminBro instance.

--- server.js
+++ server.js
@@ -5,13 +5,16 @@ const AdminBro = require('admin-bro');
 const AdminBroExpress = require('@admin-bro/express');
 const AdminBroMongoose = require('@admin-bro/mongoose');
 const dotenv = require('dotenv').config();
-const BlogPost = require('./models/blogPost');
 const blogPostsRoutes = require('./routes/blogPosts');
+//Models
+const BlogPost = require('./models/blogPost');
+const User = require('./models/user');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-//Registering the Mongoose db adapter for AdminBro
+//Registering the Mongoose db adapter for AdminBro.
+//This allows AdminBro to manage mongoose resources. 
 AdminBro.registerAdapter(AdminBroMongoose);
 
 //DB connection
@@ -25,7 +28,8 @@ mongoose.connection.once('open', () => {
   console.log('Error', err);
 });
 
-// Passing resources to AdminBro
+//Setting up blogpost body to be a quill editor instance.
+//Gives body a toolbar editor in the admin portal.
 const blogPostResourceOptions = {
   properties: {
     body: {
@@ -38,12 +42,19 @@ const blogPostResourceOptions = {
     }
   }
 };
-const adminBro = new AdminBro({
-  resources: [{ resource: BlogPost, options: blogPostResourceOptions }]
-});
-const router = AdminBroExpress.buildRouter(adminBro);
 
+//Instantiating AdminBro and passing in defined resources and options.
+const adminBro = new AdminBro({
+  resources: [
+    User,
+    { resource: BlogPost, options: blogPostResourceOptions }
+  ]
+});
+
+//Creating and using router that will handle all AdminBro routes.
+const router = AdminBroExpress.buildRouter(adminBro);
 app.use(adminBro.options.rootPath, router);
+
 app.use(bodyParser.json());
 app.use('/blogposts', blogPostsRoutes);
 
