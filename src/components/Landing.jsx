a9b98a1b841f4b068d7f876b744934fd1a90b22a
danisylvester <maria.sylvester10@gmail.com>
2021-03-02 15:00:24 -0600

    Created intersection observer service to use for animations.

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,10 +1,15 @@
 import React, {useState, useEffect, useRef } from 'react';
+import {useOnScreen} from './use-on-screen';
 import styles from './landing.module.scss';
 
 
 export const Landing = () => {
+    //Using intersection observer to detect when right panel is on the screen
+    const ref = useRef();
+    const onScreen = useOnScreen(ref);
+
+    //Detect when y axis scroll is greater than 0
     const [isScrolled, setScroll] = useState(false);
-    
     useEffect( () => {
         const onScroll = (e) => {
             setScroll(window.scrollY > 0)
@@ -13,17 +18,22 @@ export const Landing = () => {
         return () => window.removeEventListener("scroll", onScroll);
     },[isScrolled]) 
 
+
+    //Split hello text to allow for escape characters and line breaking
     const helloOne = '\<Hello';
     const helloTwo = 'World';
     const helloThree = '/\>;';
 
     return (    
         <div>
-            <div className={`${styles.leftPane} ${ isScrolled ? styles.goRight : styles}`}>
+            <div className={`${styles.leftPane} ${isScrolled ? styles.goRight : styles}`}>
                 <div className={styles.aboutColor}></div>
             </div>
 
-            <div className={styles.rightPane}>
+            <div 
+                ref={ref}
+                className={`${styles.rightPane}`}
+            >
                 <div className={styles.helloWrapper}>
                     <div className={`${styles.gridOneHello} ${isScrolled ? styles.moveHelloTextRight : styles}`}>
                         <div className="helloChild">
