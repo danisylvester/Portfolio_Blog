9a309da02c5d43d27b4f55539a3e2c2b520829b7
danisylvester <maria.sylvester10@gmail.com>
2021-03-01 21:56:59 -0600

    added animation

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -2,19 +2,11 @@ import React from 'react'
 import styles from './hello.module.scss';
 
 export const Hello = () => {
-    const helloOne = '\<Hello';
-    const helloTwo = 'World';
-    const helloThree = '/\>;';
+   
     return (
-        <div className={styles.wrapper}>
-            <div className={styles.gridOneHello}>
-                <div>
-                    {helloOne} <br></br>
-                    {helloTwo} <br></br>
-                    {helloThree}
-                </div>
-            </div>
-            <div className={styles.gridTwoColor}></div>
+        <div className={styles.leftPane}
+        >
+            Hello
         </div>
     )
 }
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,34 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect, useRef } from 'react';
 import styles from './landing.module.scss';
 import { Hello } from './Hello.jsx';
 
 
 export const Landing = () => {
     const isVisible = true;
-    
-    return (
-        <div className={styles.wrapper}>
-            <Hello></Hello>         
-            <div 
-                className={styles.aboutOuterWrapper}
-                style={{
-                    overflow: isVisible ? 'visible' : 'hidden'
-                }}
-                >
-                <div className={styles.aboutWrapper}>
-                    <div className={styles.gridOneColor}></div>
-                    <div className={styles.gridTwoAboutText}>
-                        <div className={styles.innerAboutText}>
-                            <h3 className={styles.aboutHeading}>I am Dani Sylvester</h3><br></br>
-                            <p className={styles.aboutParagraph}>I'm a website developer with a passion for creative problem solving
-                                and building beautiful things for the web.
-                            </p>
-                        </div>
-                    </div>
-                </div>                  
-            </div>
-        </div>
 
+    const [isScrolled, setScroll] = useState(false);
+    
+   
+    
+    useEffect( () => {
+        const onScroll = (e) => {
+            setScroll(window.scrollY > 0)
+        };
+        
+        window.addEventListener("scroll",onScroll,{passive:true});
+        return () => window.removeEventListener("scroll", onScroll);
+    },[isScrolled]) 
+
+    return (    
+        <div>
+            <div className={`${styles.leftPane} ${ isScrolled ? styles.goRight : styles}`}>Anything</div>
+            <div className={styles.rightPane}>Something</div>
+            <div className={styles.spacer}></div>
+            <div className={styles.everythingElse}>I AM DANI SO SMART YEA</div>
+        </div>
     )
 }
 
