7cb1b6cf53192bbe3c8046c9f7a70704279a7d45
danisylvester <maria.sylvester10@gmail.com>
2021-03-14 23:30:54 -0500

    Got rid of unnecessary logs.

--- routes/contact.js
+++ routes/contact.js
@@ -10,7 +10,6 @@ var DOMAIN = process.env.MAILGUN_DOMAIN;
 var API_KEY = process.env.MAILGUN_API_KEY;
 
 router.post('/', (req, res) => {
-    //cfconsole.log(req.body);
     const mg = mailgun({apiKey: API_KEY, domain: DOMAIN});
     const data = {
         from: req.body.email,
@@ -19,13 +18,10 @@ router.post('/', (req, res) => {
         text: 'lol'
     };
     mg.messages().send(data, (err, body) => {
-            console.log('made it here')
             if(err){
-                console.log('dungoofed')
                 res.status(500).send({error: err})
             }
             res.send('Message was sent successfully!');
-            console.log(body);
         })
 });
 
