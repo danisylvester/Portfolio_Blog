47208bdc93584c550562c9af418b21738ced85d8
danisylvester <maria.sylvester10@gmail.com>
2021-03-28 18:04:31 -0500

    Changed mouseover effect to only render if device is over 768px.

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,80 +1,149 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import styles from './projects.module.scss'
-import Tilty from 'react-tilty';
-
-
+import VanillaTilt from 'vanilla-tilt';
 
 export const Projects = () => {
+
+    // Mouse over tilt effect on tablet and computer views.
+    useEffect(() => {
+        if(window.innerWidth >= 768){
+            VanillaTilt.init(document.querySelectorAll('.tilt'),{
+                max: 5,
+                speed: 100,
+                transition: true,
+                reset: true,
+                easing: "cubic-bezier(.03,.98,.52,.99)",
+                axis: 'x'
+            });
+        }
+    })
      return (
+        <div className={styles.gridContainer}>
+
         <div className={styles.bkg}>
             <div className={styles.headingWrapper}>
                 <div className={styles.mainHeading}>
-                    <h3>Recent Projects</h3>
+                    <h3 className={styles.purpleText}>Recent</h3>
+                    <h3 className={styles.projHeader}>Projects</h3>
                 </div>
-                <div className={styles.mainHeading}>
-                    <p>Check out my recent projects.</p>
+                <div className={styles.headingParagraph}>
+                    <p>Check out some of my featured work. If you’re interested in seeing more, reach out and say hello!</p>
                 </div>
             </div>
             <div className={styles.projectWrapper}>
-                <Tilty  reverse axis="x" scale={1} perspective={3500} reset={true} styles="cubic-bezier(.03,.98,.52,.99)">
-                    <div className={styles.gridCard}>
-                        <div className={styles.gridTopShape}>
-                            <img className={styles.triangles} src='/src/assets/sns-top-triangle.png'></img>
+                <div className='tilt'>
+                    <div className={`${styles.gridCard} ${styles.horizGridCard}`}>
+                        <div className={`${styles.gridDevices} ${styles.horizGridDevices}`}>
+                        <div className={styles.gradientColorOne}>
+                                <div className={styles.bkgPattern}>
+                                    <img className={styles.deviceImg} src='/src/assets/sns-devices.png'></img>
+                                </div>
+                            </div>
                         </div>
-                        <div className={styles.gridDevices}>
-                            <img className={styles.deviceImg} src='/src/assets/sns-devices.png'></img>
+                        <div className={`${styles.gridTitle} ${styles.horizGridTitle}`}>
+                            <h3 className={styles.projTitle}>Portfolio</h3>
                         </div>
-                        <div className={styles.gridTitle}>
+                        <div className={`${styles.gridSkills} ${styles.horizGridSkills}`}>
+                            <div className={`${styles.skill} ${styles.skillsOne}`}>React</div>
+                            <div className={`${styles.skill} ${styles.skillsOne}`}>JavaScript</div>
+                            <div className={`${styles.skill} ${styles.skillsOne}`}>Node.js</div>
+                            <div className={`${styles.skill} ${styles.skillsOne}`}>MongoDB</div>
+                            <div className={`${styles.skill} ${styles.skillsOne}`}>Mongoose</div>
+                            <div className={`${styles.skill} ${styles.skillsOne}`}>AdminBro CMS</div>
+                            <div className={`${styles.skill} ${styles.skillsOne}`}>SCSS</div>
+                            <div className={`${styles.skill} ${styles.skillsOne}`}>HTML</div>
+                        </div>
+                        <div className={`${styles.gridViewBtn} ${styles.horizGridViewBtn}`}>
+                            <button className={`${styles.viewBtn} ${styles.btnOne}`}>
+                                <span className={styles.btnText}>View Project</span>
+                            </button>
+                        </div>
+                    </div> 
+                 </div>
+            </div>
+            <div className={styles.projectWrapper}>
+                <div className='tilt'>
+                    <div className={`${styles.gridCard} ${styles.horizGridCard}`}>
+                        <div className={`${styles.gridDevices} ${styles.horizGridDevices}`}>
+                            <div className={styles.gradientColorTwo}>
+                                <div className={styles.bkgPattern}>
+                                    <img className={styles.deviceImg} src='/src/assets/sns-devices.png'></img>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={`${styles.gridTitle} ${styles.horizGridTitle}`}>
                             <h3 className={styles.projTitle}>SNS Virtual Solutions</h3>
                         </div>
-                        <div className={styles.gridSkills}>
-                            <div className={styles.skill}>Angular</div>
-                            <div className={styles.skill}>TypeScript</div>
-                            <div className={styles.skill}>Node.js</div>
-                            <div className={styles.skill}>SCSS</div>
-                            <div className={styles.skill}>HTML</div>
+                        <div className={`${styles.gridSkills} ${styles.horizGridSkills}`}>
+                            <div className={`${styles.skill} ${styles.skillsTwo}`}>Angular</div>
+                            <div className={`${styles.skill} ${styles.skillsTwo}`}>TypeScript</div>
+                            <div className={`${styles.skill} ${styles.skillsTwo}`}>Node.js</div>
+                            <div className={`${styles.skill} ${styles.skillsTwo}`}>SCSS</div>
+                            <div className={`${styles.skill} ${styles.skillsTwo}`}>HTML</div>
                         </div>
-                        <div className={styles.gridViewBtn}>
-                            <button className={styles.viewBtn}>
+                        <div className={`${styles.gridViewBtn} ${styles.horizGridViewBtn}`}>
+                            <button  className={`${styles.viewBtn} ${styles.btnTwo}`}>
                                 <span className={styles.btnText}>View Project</span>
                             </button>
                         </div>
-                        <div className={styles.gridBtmSNSShapes}>
-                            <img className={styles.bottomShapes} src='/src/assets/sns-bottom-shapes.png'></img>
-                        </div>
                     </div> 
-                 </Tilty>
+                </div>
             </div>
-            <div className={styles.projectWrapper}>
-                <Tilty reverse axis="x" scale={1} perspective={3500} reset={true} styles="cubic-bezier(.03,.98,.52,.99)">
+            <div className={`${styles.verticalWrap}`}>
+                <div className='tilt'>
                     <div className={styles.gridCard}>
                         <div className={styles.gridDevices}>
-                            <img className={styles.deviceImg} src='/src/assets/uai-devices.png'></img>
+                            <div className={styles.gradientColorOne}>
+                                <div className={styles.bkgPattern}>
+                                    <img className={styles.deviceImg} src='/src/assets/uai-devices.png'></img>
+                                </div>
+                            </div>
                         </div>
                         <div className={styles.gridTitle}>
-                            <h3 className={styles.projTitle}>UAI Middle East</h3>
+                            <h3 className={styles.projTitle}>UAI MIDDLE EAST</h3>
                         </div>
                         <div className={styles.gridSkills}>
-                            <div className={styles.skill}>Angular</div>
-                            <div className={styles.skill}>TypeScript</div>
-                            <div className={styles.skill}>Node.js</div>
-                            <div className={styles.skill}>SCSS</div>
-                            <div className={styles.skill}>HTML</div>
+                            <div className={`${styles.skill} ${styles.skillsOne}`}>Angular</div>
+                            <div className={`${styles.skill} ${styles.skillsOne}`}>TypeScript</div>
+                            <div className={`${styles.skill} ${styles.skillsOne}`}>Node.js</div>
+                            <div className={`${styles.skill} ${styles.skillsOne}`}>SCSS</div>
+                            <div className={`${styles.skill} ${styles.skillsOne}`}>HTML</div>
                         </div>
                         <div className={styles.gridViewBtn}>
-                            <button className={styles.viewBtn}>
+                            <button  className={`${styles.viewBtn} ${styles.btnOne}`}>
                                 <span className={styles.btnText}>View Project</span>
                             </button>
                         </div>
-                        <div className={styles.gridBtmUAIShapes}>
-                            <img className={styles.bottomShapes} src='/src/assets/uai-left-shapes.png'></img>
+                    </div> 
+                </div>
+            </div>
+            <div className={`${styles.verticalWrap}`}>
+                <div className='tilt'>
+                    <div className={styles.gridCard}>
+                        <div className={styles.gridDevices}>
+                            <div className={styles.gradientColorThree}>
+                                <div className={styles.bkgPattern}>
+                                    <img className={styles.deviceImg} src='/src/assets/polyglot.png'></img>
+                                </div>
+                            </div>
                         </div>
-                        <div className={styles.gridUAITriangle}>
-                            <img className={styles.triangles} src='/src/assets/uai-right-triangle.png'></img>
+                        <div className={styles.gridTitle}>
+                            <h3 className={styles.projTitle}>Polyglot Chrome Extension</h3>
+                        </div>
+                        <div className={styles.gridSkills}>
+                            <div className={`${styles.skill} ${styles.skillsThree}`}>JavaScript</div>
+                            <div className={`${styles.skill} ${styles.skillsThree}`}>CSS</div>
+                            <div className={`${styles.skill} ${styles.skillsThree}`}>HTML</div>
+                        </div>
+                        <div className={styles.gridViewBtn}>
+                            <button  className={`${styles.viewBtn} ${styles.btnThree}`}>
+                                <span className={styles.btnText}>View Project</span>
+                            </button>
                         </div>
                     </div> 
-                </Tilty>
+                </div>
             </div>
+        </div>
         </div>
     )
 }
