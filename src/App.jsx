8559a35090e0dcfb5b12369a89fae07ea383f2ed
danisylvester <maria.sylvester10@gmail.com>
2021-04-05 11:00:13 -0500

    Got rid of routes no longer used. Added blogpost route.

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,7 @@ import './App.scss';
 import { Menu } from './components/Menu/Menu.jsx';
 import { BottomMenu } from './components/BottomMenu/BottomMenu.jsx'
 import { Home } from './components/Home/Home.jsx';
-import { Projects } from './components/Projects/Projects.jsx';
-import { Blog } from './components/Blog/Blog.jsx';
-import { Contact } from './components/Contact/Contact.jsx';
+import BlogPost from './components/BlogPost/BlogPost.jsx';
 
 const App = () => {
   return (
@@ -14,9 +12,7 @@ const App = () => {
         <Menu></Menu>
         <Switch>
           <Route exact path='/' component={Home}></Route>
-          <Route path='/projects' component={Projects}></Route>
-          <Route path='/blogposts' component={Blog}></Route>
-          <Route path='/contact' component={Contact}></Route>
+          <Route path='/blogpost/:blogID' component={BlogPost}></Route>
         </Switch>
         <BottomMenu></BottomMenu>
     </>
