72fbecfb70584a23e1f73ffd0b85654e2d935190
danisylvester <maria.sylvester10@gmail.com>
2021-04-08 15:18:32 -0500

    Changed welcome text on login page and small formatting updates.

--- adminPortal/adminserver.js
+++ adminPortal/adminserver.js
@@ -1,61 +1,66 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const AdminBro = require('admin-bro');
-const AdminBroExpress = require('@admin-bro/express');
-const AdminBroMongoose = require('@admin-bro/mongoose');
-const bcrypt = require('bcrypt');
-const dotenv = require('dotenv').config();
-const BlogPost = require('./models/blogPost.js');
-const User = require('./models/user');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const AdminBro = require("admin-bro");
+const AdminBroExpress = require("@admin-bro/express");
+const AdminBroMongoose = require("@admin-bro/mongoose");
+const bcrypt = require("bcrypt");
+const dotenv = require("dotenv").config();
+const BlogPost = require("./models/blogPost.js");
+const User = require("./models/user");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 //Registering the Mongoose db adapter for AdminBro.
-//This allows AdminBro to manage mongoose resources. 
+//This allows AdminBro to manage mongoose resources.
 AdminBro.registerAdapter(AdminBroMongoose);
 
 //DB connection
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology:true
+  useUnifiedTopology: true,
 });
-mongoose.connection.once('open', () => {
-  console.log('db is connected');
-}).on('error', () => {
-  console.log('Error', err);
-});
+mongoose.connection
+  .once("open", () => {
+    console.log("db is connected");
+  })
+  .on("error", () => {
+    console.log("Error", err);
+  });
 
 //Defining options for AdminBro user and blogpost resources
 //Hiding the encrypted password, so you cannot see this from Admin portal.
 //And adding a new virtual password property that is only shown on edit page.
 const userResourceOptions = {
   properties: {
-    encryptedPassword : {
-      isVisible: false
+    encryptedPassword: {
+      isVisible: false,
     },
-    password: { 
-      type: 'string',
+    password: {
+      type: "string",
       isVisible: {
-        list: false, edit: true, filter: false, show: false
-      }
-    }
+        list: false,
+        edit: true,
+        filter: false,
+        show: false,
+      },
+    },
   },
   actions: {
     new: {
       before: async (request) => {
-        if(request.payload.password) {
+        if (request.payload.password) {
           request.payload = {
             ...request.payload,
             encryptedPassword: await bcrypt.hash(request.payload.password, 10),
-            password: undefined
-          }
+            password: undefined,
+          };
         }
         return request;
-      }
-    }
-  }
+      },
+    },
+  },
 };
 
 //Setting up blogpost title & body properties to be a quill editor instances.
@@ -63,45 +68,68 @@ const userResourceOptions = {
 const blogPostResourceOptions = {
   properties: {
     body: {
-      type: 'richtext',
+      type: "richtext",
       custom: {
         modules: {
-          toolbar: [['bold', 'italic'], ['link', 'image']]
-        }
-      }
+          toolbar: [
+            ["bold", "italic"],
+            ["link", "image"],
+          ],
+        },
+      },
     },
     title: {
-      type: 'richtext',
+      type: "richtext",
       custom: {
         modules: {
-          toolbar: [['bold', 'italic'], ['link', 'image']]
-        }
-      }
-    }
-  }
+          toolbar: [
+            ["bold", "italic"],
+            ["link", "image"],
+          ],
+        },
+      },
+    },
+  },
+};
+
+const locale = {
+  translations: {
+    labels: {
+      loginWelcome: "Hi Dani!",
+    },
+    messages: {
+      loginWelcome: "",
+    },
+  },
 };
 
 //Instantiating AdminBro and passing in defined resources and options.
 const adminBro = new AdminBro({
   resources: [
     { resource: User, options: userResourceOptions },
-    { resource: BlogPost, options: blogPostResourceOptions }
-  ]
+    { resource: BlogPost, options: blogPostResourceOptions },
+  ],
+  locale,
+  branding: {
+    companyName: "Hi Dani!",
+    softwareBrothers: false,
+    logo: "",
+  },
 });
 
 //Creating and using router that will handle all AdminBro routes.
 const router = AdminBroExpress.buildAuthenticatedRouter(adminBro, {
   authenticate: async (email, password) => {
-    const user = await User.findOne({email});
-    if(user) {
+    const user = await User.findOne({ email });
+    if (user) {
       const matched = await bcrypt.compare(password, user.encryptedPassword);
-      if(matched){
+      if (matched) {
         return user;
       }
     }
     return false;
   },
-  cookiePassword: 'some-secret-password-used-to-secure-cookie' //temporary 
+  cookiePassword: "some-secret-password-used-to-secure-cookie", //temporary
 });
 app.use(adminBro.options.rootPath, router);
 
